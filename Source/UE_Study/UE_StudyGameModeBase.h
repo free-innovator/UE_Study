a75cde2318c0a2fa47928101b549ba94d2d60e36
NoName <qkrtpwhd2000@naver.com>
2021-10-07 16:43:34 +0900

    init commit

---
+++ Source/UE_Study/UE_StudyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE_StudyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_STUDY_API AUE_StudyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
